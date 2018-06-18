50b38b022700464e7b37ecab7c5fd6f575ce21c7
arthy <arthy@DESKTOP-NTM5730.attlocal.net>
2018-06-17 21:18:16 -0700

    Sample Library commit

---
+++ sampleselenium/src/sample/libs/SampleLibs.java
@@ -0,0 +1,10 @@
+package sample.libs;
+
+public class SampleLibs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("In libs");
+	}
+
+}
